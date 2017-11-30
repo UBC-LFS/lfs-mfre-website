5eab2f429124fe4212c2a66b85202c0ab51e4867
Justin Lee <justinlee0022@gmail.com>
2017-11-30 12:05:31 -0800

    Got company link working

--- work-after-mfre.js
+++ work-after-mfre.js
@@ -1,4 +1,10 @@
 /* global jQuery */
 jQuery(document).ready(function ($) {
   $('.mfre-slider').slick()
+
+  const companyWebsite = document.getElementById('company-website').innerText
+
+  const wrapper = `<a href='${companyWebsite}' style='display: inherit;'></a>`
+
+  $('#company-name').wrap(wrapper)
 })
