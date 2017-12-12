218499becf7f46d89aa369b9ff02b4273bf4b292
Justin Lee <justinlee0022@gmail.com>
2017-12-12 09:32:48 -0800

    fixed grad-showcase slider

--- grad-showcase.js
+++ grad-showcase.js
@@ -15,18 +15,13 @@ jQuery(document).ready(function ($) {
   $('#myCarousel2').swipeleft(function () {
     $('#myCarousel2').carousel('next')
   })
-})
 
-/* global jQuery */
-jQuery(document).ready(function ($) {
-  const span12 = '<div class="span12" style="margin-left:0px;"></div>'
-
-  const profiles = document.getElementsByClassName('profiles')
-  Array.from(profiles).map((x, i) => (x.className += (' level' + Math.floor(i / 4))))
-
-  for (let i = 0; i < profiles.length; i++) {
-    if (i % 4 === 0) {
-      $('.level' + Math.floor(i / 4)).wrapAll(span12)
-    }
+  const wrap = (urlClass, targetClass) => {
+    const elements = document.getElementsByClassName(urlClass)
+    const urls = Array.from(elements).map(x => x.innerText.trim())
+    const targets = document.getElementsByClassName(targetClass)
+    Array.from(targets).map((x, i) => $(x).wrap(`<a href='${urls[i]}' style='display: inherit;'></a>`))
   }
+
+  wrap('carousel-link-source', 'carousel-link-target')
 })
